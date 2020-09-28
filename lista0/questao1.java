18c4172782ea99ec232686d371d26d34547aa16e
Apollo Samuel <iamapollosamuel@gmail.com>
2020-09-28 19:58:46 -0300

    Add files via upload

--- lista0/questao1.java
+++ lista0/questao1.java
@@ -1,19 +1,12 @@
 package lista0;
 
-import java.util.Scanner;
 
 public class questao1 {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Digite um número: ");
-		double numero = input.nextDouble();
+	
+		Lista0_OO.questao01(-50);
 
-		double absoluto = Math.abs(numero);
-		System.out.println("Número absoluto: " + absoluto);
-		
-		input.close();
 	}
 
 }
--- lista0/questao10.java
+++ lista0/questao10.java
@@ -4,36 +4,8 @@ public class questao10 {
 
 	public static void main(String[] args) {
 		
-		boolean a = false;
-		boolean b = true;
-		char op = 'o';
-		boolean resultado = true;
-
-		//Executa a lógica booleana
-
-		if (op == 'A' || op == 'a'){ //AND
-
-			resultado = (a & b);
-
-		}else if(op == 'O' || op == 'o') { // OR
-
-			resultado = (a | b);
-
-		}else if(op == 'X' || op == 'x') { //XOR
-
-			resultado = (a ^ b);
-
-		}else if(op == 'N' || op == 'n') { //NAND
-
-			resultado = !(a & b);
-
-		}else {
-			System.out.println("Operador inválido!");
-		}
-
-
-		System.out.println(a + " " + op + " " + b + " " + " : " + resultado);
-
+		Lista0_OO.questao10(true, true, 'a');
+		
 	}
 
 }
