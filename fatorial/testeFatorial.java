fbe67c026669a2a8809f4b8a3f421f86d6a3e9b9
Apollo Samuel <iamapollosamuel@gmail.com>
2020-10-04 21:37:51 -0300

    Add files via upload

--- fatorial/testeFatorial.java
+++ fatorial/testeFatorial.java
@@ -3,8 +3,12 @@ package fatorial;
 public class testeFatorial {
 
 	public static void main(String[] args) {
-			
+		
+		//Fatorial Recursivo
 		System.out.println(fatorialRecursivo.fatorialRecursivo(5));
+		
+		//Fatorial Iterativo
+		System.out.println(fatorialIterativo.fatorialIterativo(5));
 
 	}
 
