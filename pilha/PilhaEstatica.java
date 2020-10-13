89369d33ab1dfbb0d1b7750032cf200fa21689b0
Apollo Samuel <iamapollosamuel@gmail.com>
2020-10-13 20:41:55 -0300

    complemento da pilha estatica
    complemento da pilha estatica

--- pilha/PilhaEstatica.java
+++ pilha/PilhaEstatica.java
@@ -30,5 +30,50 @@ public class PilhaEstatica {
 	public int tamanho() {
 		return(capacidade);
 	}
+	
+	public void empilhar(Object novoItem) {
+		if(topo == capacidade) { //verifica se está cheia
+			System.err.println("Erro: Capacidade da pilha foi excedida!");
+		}else {
+			itemArray[topo++] = novoItem; //adiciona um novo item no Array e adiciona mais 1 item no contador
+			contador++;
+		}
+	}
+	
+	public Object desempilhar() {
+		Object x = null; // atribui um vazio para o topo da pilha 
+		if (vazia() ) {
+			System.err.println("Erro: Pilha vazia!"); //verifica se esta vazia
+		}else {
+			x = itemArray[-- topo]; // diminui o item do topo 
+			contador--; //abate no contador
+		}
+		return(x);
+	}
+	
+	
+	public Object consultarTopo() {
+		Object x = null; // atribui um vazio para o topo da pilha 
+		if(vazia() ) {
+			System.err.println("Erro: Pilha vazia!"); //verifica se esta vazia
+		}else {
+			x = itemArray[topo-1]; //topo é sempre null, -1 serve para consultar o segundo item da lista
+		}
+		return(x);
+	}
+	
+	//Imprimir a pilha
+	public String toString() {
+		String pilhaCompleta = "";
+		if(vazia() ) {
+			pilhaCompleta = null;
+		}else {
+			for(int i = topo-1; i >= 0; i--) { //mostra de cima pra baixo 
+				pilhaCompleta = pilhaCompleta + "\n" +itemArray[i];
+			}
+		}
+		return(pilhaCompleta);
+	}
+	
 
 }
